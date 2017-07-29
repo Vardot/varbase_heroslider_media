3b03742aba90d854f5d58a84f37755200c25d3c7
git <git@3354734.no-reply.drupal.org>
2017-07-29 12:42:25 +0300

    Issue #2897867 by ahmadhalah: Make hero slider working with youtube

--- js/varbase_hero_slider_media.scripts.js
+++ js/varbase_hero_slider_media.scripts.js
@@ -6,14 +6,14 @@
 (function ($, _, Drupal, drupalSettings) {
   "use strict";
 
-  Drupal.behaviors.varbaseHeroSliderMedia = {
+  Drupal.behaviors.varbaseHeroSliderMedia_vimeo = {
     attach: function (context) {
       // On before slide change.
       $('.slick--view--varbase-heroslider-media .slick__slider', context).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-          var currentSlideObject = $('.slide--' + currentSlide + '.slick-active');
-          var nextSlideObject = $('.slide--' + nextSlide);
-          var currentIframe = currentSlideObject.find('.video-embed-field-responsive-video iframe', context);
-          var nextIframe = nextSlideObject.find('.video-embed-field-responsive-video iframe', context);
+        var currentSlideObject = $('.slide--' + currentSlide + '.slick-active');
+        var nextSlideObject = $('.slide--' + nextSlide);
+        var currentIframe = currentSlideObject.find('.video-embed-field-responsive-video iframe', context);
+        var nextIframe = nextSlideObject.find('.video-embed-field-responsive-video iframe', context);
 
         if (currentIframe.length !== 0) {
           var currentPlayer = $f(currentIframe[0]);
@@ -29,44 +29,111 @@
         }
       });
 
-        // When first slide has a video (Pause the slider and play the video).
-        $('.slick--view--varbase-heroslider-media .video-embed-field-responsive-video iframe').on("load", function () {
-          var firstSlideVideo = $('.slick__slider .slick-active').find('.media-video').length !== 0;
-          if (firstSlideVideo) {
-            $('.slick__slider').slick('slickPause');
-            player.api("play");
+      // When first slide has a video (Pause the slider and play the video).
+      $('.slick--view--varbase-heroslider-media .video-embed-field-responsive-video iframe').on("load", function () {
+        var firstSlideVideo = $('.slick__slider .slick-active').find('.media-video').length !== 0;
+        if (firstSlideVideo) {
+          $('.slick__slider').slick('slickPause');
+          player.api("play");
+        }
+      });
+
+      // Vimeo variable.
+      if ($('.slick--view--varbase-heroslider-media .video-embed-field-responsive-video iframe').length > 0) {
+        var iframe = $('.video-embed-field-responsive-video iframe')[0];
+        var player = $f(iframe);
+
+        // When the player is ready, add listeners for pause, finish,
+        // and playProgress.
+        player.addEvent('ready', function () {
+          player.addEvent('pause', onPause);
+          player.addEvent('finish', onFinish);
+          player.addEvent('playProgress', onPlayProgress);
+        });
+      }
+
+      // Play when paused.
+      function onPause() {
+        $('.slick__slider').slick('slickPlay');
+      }
+
+      // Play when finished.
+      function onFinish() {
+        $('.slick__slider').slick('slickPlay');
+      }
+
+      // Puse on play prgress.
+      function onPlayProgress() {
+        $('.slick__slider').slick('slickPause');
+      }
+
+    }
+  };
+
+  Drupal.behaviors.varbaseHeroSliderMedia_youtube = {
+    attach: function (context, settings) {
+      $(window).on('load', function () {
+
+        //Youtube API
+        var yotubePlayer;
+        yotubePlayer = new YT.Player('youtubeVideo', {
+          events: {
+            'onStateChange': onPlayerStateChange,
+            'onReady': onPlayerReady
           }
         });
 
-        // Vimeo variable.
-        if ($('.slick--view--varbase-heroslider-media .video-embed-field-responsive-video iframe').length > 0) {
-          var iframe = $('.video-embed-field-responsive-video iframe')[0];
-          var player = $f(iframe);
-
-          // When the player is ready, add listeners for pause, finish,
-          // and playProgress.
-          player.addEvent('ready', function () {
-            player.addEvent('pause', onPause);
-            player.addEvent('finish', onFinish);
-            player.addEvent('playProgress', onPlayProgress);
-          });
+        //Play youtube video on ready
+        function onPlayerReady() {
+          var firstSlideVideo = $('.varbase-heroslider-media .slick__slider .slick-active').find('.media-video').length !== 0;
+          if (firstSlideVideo) {
+            $('.varbase-heroslider-media .slick__slider').slick('slickPause');
+            yotubePlayer.playVideo();
+          } else {// if hero slider has one Slide
+            var onlySlide = $('.varbase-heroslider-media .slick').find('.media-video').length !== 0;
+            if (onlySlide) {
+              yotubePlayer.playVideo();
+            }
+          }
         }
 
-        // Play when paused.
-        function onPause() {
-          $('.slick__slider').slick('slickPlay');
+        //video status
+        function onPlayerStateChange(event) {
+          if (event.data === 0) { // On finish
+            $('.varbase-heroslider-media .slick__slider').slick('slickPlay');
+          } else if (event.data === 1) { // On playing
+            $('.varbase-heroslider-media .slick__slider').slick('slickPause');
+          } else if (event.data === 2) { // Onpause
+            $('.varbase-heroslider-media .slick__slider').slick('slickPause');
+          }
         }
 
-        // Play when finished.
-        function onFinish() {
-          $('.slick__slider').slick('slickPlay');
-        }
+        $('.slick--view--varbase-heroslider-media .slick__slider', context).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+          var currentSlideObject = $('.varbase-heroslider-media .slide--' + currentSlide + '.slick-active');
+          var nextSlideObject = $('.varbase-heroslider-media .slide--' + nextSlide);
+          var currentIframe = currentSlideObject.find('.video-embed-field-responsive-video #youtubeVideo', context);
+          var nextIframe = nextSlideObject.find('.video-embed-field-responsive-video #youtubeVideo', context);
 
-        // Puse on play prgress.
-        function onPlayProgress() {
-          $('.slick__slider').slick('slickPause');
-        }
+          if (currentIframe.length !== 0) {
+            yotubePlayer.a = currentIframe[0];
+            yotubePlayer.pauseVideo();
+          }
 
+          if (nextIframe.length !== 0) {
+            yotubePlayer.a = nextIframe[0];
+            yotubePlayer.playVideo();
+          }
+        });
+
+        // When first slide has a video (Pause the slider and play the video).
+        $('.slick--view--varbase-heroslider-media .video-embed-field-responsive-video #youtubeVide').on("load", function () {
+          var firstSlideVideo = $('.varbase-heroslider-media .slick__slider .slick-active').find('.media-video').length !== 0;
+          if (firstSlideVideo) {
+            $('.varbase-heroslider-media .slick__slider').slick('slickPause');
+            yotubePlayer.playVideo();
+          }
+        });
+      });
     }
   };
 })(window.jQuery, window._, window.Drupal, window.drupalSettings);
