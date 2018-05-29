4d772cfd3a87f6c5c1d6dc577948828e389f6fab
Rajab Natshah <rajabn@gmail.com>
2018-05-29 03:38:05 +0300

    Issue #2975916: Removed internal [youtube-player, vimeo-player]  libraries as we will use varbase_media/youtube_player and varbase_media/vimeo_player libraries , and added the support for Local hosted videos with events in the slick slides

--- js/varbase_hero_slider_media.scripts.js
+++ js/varbase_hero_slider_media.scripts.js
@@ -13,16 +13,16 @@
       $('.slick--view--varbase-heroslider-media .slick__slider', context).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
         var currentSlideObject = $('.slide--' + currentSlide + '.slick-active');
         var nextSlideObject = $('.slide--' + nextSlide);
-        var currentIframe = currentSlideObject.find('.video-embed-field-responsive-video iframe[src*="vimeo.com"]', context);
-        var nextIframe = nextSlideObject.find('.video-embed-field-responsive-video iframe[src*="vimeo.com"]', context);
+        var currentIframe = currentSlideObject.find('.varbase-video-player iframe[src*="vimeo.com"]', context);
+        var nextIframe = nextSlideObject.find('.varbase-video-player iframe[src*="vimeo.com"]', context);
 
         if (currentIframe.length !== 0) {
-          var currentPlayer = new Vimeo.Player(currentIframe[0]);
+          var currentPlayer = new Vimeo.Player(currentIframe.get(0));
           currentPlayer.pause();
         }
 
         if (nextIframe.length !== 0) {
-          var nextPlayer = new Vimeo.Player(nextIframe[0]);
+          var nextPlayer = new Vimeo.Player(nextIframe.get(0));
           nextPlayer.on('pause', onPause);
           nextPlayer.on('ended', onFinish);
           nextPlayer.on('play', onPlayProgress);
@@ -31,7 +31,7 @@
       });
 
       // When first slide has a video (Pause the slider and play the video).
-      $('.slick--view--varbase-heroslider-media .video-embed-field-responsive-video iframe[src*="vimeo.com"]').on("load", function () {
+      $('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"]').on("load", function () {
         var firstSlideVideo = $('.slick__slider .slick-active').find('.media-video').length !== 0;
         if (firstSlideVideo) {
           $('.slick__slider').slick('slickPause');
@@ -40,8 +40,8 @@
       });
 
       // Vimeo variable.
-      if ($('.slick--view--varbase-heroslider-media .video-embed-field-responsive-video iframe[src*="vimeo.com"]').length > 0) {
-        var iframe = $('.video-embed-field-responsive-video iframe[src*="vimeo.com"]')[0];
+      if ($('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"]').length > 0) {
+        var iframe = $('.varbase-video-player iframe[src*="vimeo.com"]').get(0);
         var player = new Vimeo.Player(iframe);
 
         // When the player is ready, add listeners for pause, finish,
@@ -109,28 +109,91 @@
         $('.slick--view--varbase-heroslider-media .slick__slider', context).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
           var currentSlideObject = $('.slick--view--varbase-heroslider-media .slide--' + currentSlide + '.slick-active');
           var nextSlideObject = $('.slick--view--varbase-heroslider-media .slide--' + nextSlide);
-          var currentIframe = currentSlideObject.find('.video-embed-field-responsive-video #youtubeVideo', context);
-          var nextIframe = nextSlideObject.find('.video-embed-field-responsive-video #youtubeVideo', context);
+          var currentIframe = currentSlideObject.find('.varbase-video-player #youtubeVideo', context);
+          var nextIframe = nextSlideObject.find('.varbase-video-player #youtubeVideo', context);
 
           if (currentIframe.length !== 0) {
-            yotubePlayer.a = currentIframe[0];
+            yotubePlayer.a = currentIframe.get(0);
             yotubePlayer.pauseVideo();
           }
 
           if (nextIframe.length !== 0) {
-            yotubePlayer.a = nextIframe[0];
+            yotubePlayer.a = nextIframe.get(0);
             yotubePlayer.playVideo();
           }
         });
 
         // When first slide has a video (Pause the slider and play the video).
-        $('.slick--view--varbase-heroslider-media .video-embed-field-responsive-video #youtubeVide').on("load", function () {
+        $('.slick--view--varbase-heroslider-media .varbase-video-player #youtubeVide').on("load", function () {
           var firstSlideVideo = $('.slick--view--varbase-heroslider-media .slick__slider .slick-active').find('.media-video').length !== 0;
           if (firstSlideVideo) {
             $('.slick--view--varbase-heroslider-media .slick__slider').slick('slickPause');
             yotubePlayer.playVideo();
           }
         });
+      });
+    }
+  };
+
+  Drupal.behaviors.varbaseHeroSliderMedia_local_video = {
+    attach: function (context, settings) {
+      $(window).on('load', function () {
+        // On before slide change.
+        $('.slick--view--varbase-heroslider-media .slick__slider', context).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+          var currentSlideObject = $('.slide--' + currentSlide + '.slick-active');
+          var nextSlideObject = $('.slide--' + nextSlide);
+          var currentVideo = currentSlideObject.find('.varbase-video-player video', context);
+          var nextVideo = nextSlideObject.find('.varbase-video-player video', context);
+
+          if (currentVideo.length !== 0) {
+            var currentPlayer = currentVideo.get(0);
+            currentPlayer.pause();
+          }
+
+          if (nextVideo.length !== 0) {
+            var nextPlayer = nextVideo.get(0);
+            nextPlayer.onpause = onPause();
+            nextPlayer.onended = onFinish();
+            nextPlayer.onplay = onPlayProgress();
+            nextPlayer.play();
+          }
+        });
+
+        // When first slide has a video (Pause the slider and play the video).
+        $('.slick--view--varbase-heroslider-media .varbase-video-player video').on("load", function () {
+          var firstSlideVideo = $('.slick__slider .slick-active').find('.media-video').length !== 0;
+          if (firstSlideVideo) {
+             $('.slick--view--varbase-heroslider-media .slick__slider').slick('slickPause');
+            var player = $('.slick--view--varbase-heroslider-media .varbase-video-player video').get(0);
+            player.play();
+          }
+        });
+
+        // Local Video variable.
+        if ($('.slick--view--varbase-heroslider-media .varbase-video-player video').length > 0) {
+          var player = $('.slick--view--varbase-heroslider-media .varbase-video-player video').get(0);
+
+          // When the player is ready, add listeners for pause, finish,
+          // and playProgress.
+            player.onpause = onPause();
+            player.onended = onFinish();
+            player.onplay = onPlayProgress();
+        }
+
+        // Play when paused.
+        function onPause() {
+          $('.slick__slider').slick('slickPlay');
+        }
+
+        // Play when finished.
+        function onFinish() {
+          $('.slick__slider').slick('slickPlay');
+        }
+
+        // Pause on play prgress.
+        function onPlayProgress() {
+          $('.slick__slider').slick('slickPause');
+          }
       });
     }
   };
