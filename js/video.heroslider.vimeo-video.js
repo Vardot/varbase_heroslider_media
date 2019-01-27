a8a2fa533315448ca804b2b31987906fe59283b3
Rajab Natshah <rajabn@gmail.com>
2019-01-27 12:55:55 +0200

    Issue #3028739: Fix issue with single slide height on full content and single slick slide and the center of video players

--- js/video.heroslider.vimeo-video.js
+++ js/video.heroslider.vimeo-video.js
@@ -6,23 +6,23 @@
 (function ($, _, Drupal, drupalSettings) {
   "use strict";
 
-  Drupal.behaviors.varbaseHeroSliderMedia_vimeo = {
+Drupal.behaviors.varbaseHeroSliderMedia_vimeo = {
     attach: function (context, settings) {
 
       // On before slide change.
       $('.slick--view--varbase-heroslider-media .slick__slider', context).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
         var currentSlideObject = $('.slide--' + currentSlide + '.slick-active');
         var nextSlideObject = $('.slide--' + nextSlide);
-        var currentIframe = currentSlideObject.find('.varbase-video-player iframe[src*="vimeo.com"]', context);
-        var nextIframe = nextSlideObject.find('.varbase-video-player iframe[src*="vimeo.com"]', context);
+        var currentIframe = currentSlideObject.find('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"] iframe', context);
+        var nextIframe = nextSlideObject.find('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"] iframe', context);
 
         if (currentIframe.length !== 0) {
-          var currentPlayer = new Vimeo.Player(currentIframe.get(0));
+          var currentPlayer = new Vimeo.Player(currentIframe[0]);
           currentPlayer.pause();
         }
 
         if (nextIframe.length !== 0) {
-          var nextPlayer = new Vimeo.Player(nextIframe.get(0));
+          var nextPlayer = new Vimeo.Player(nextIframe[0]);
           nextPlayer.on('pause', onPause);
           nextPlayer.on('ended', onFinish);
           nextPlayer.on('play', onPlayProgress);
@@ -31,17 +31,19 @@
       });
 
       // When first slide has a video (Pause the slider and play the video).
-      $('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"]').on("load", function () {
+      $('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"] iframe').on("load", function () {
         var firstSlideVideo = $('.slick__slider .slick-active').find('.media-video').length !== 0;
         if (firstSlideVideo) {
           $('.slick__slider').slick('slickPause');
+          var iframe = $('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"] iframe').get(0);
+          var player = new Vimeo.Player(iframe);
           player.play();
         }
       });
 
       // Vimeo variable.
-      if ($('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"]').length > 0) {
-        var iframe = $('.varbase-video-player iframe[src*="vimeo.com"]').get(0);
+      if ($('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"] iframe').length > 0) {
+        var iframe = $('.slick--view--varbase-heroslider-media .varbase-video-player iframe[src*="vimeo.com"] iframe').get(0);
         var player = new Vimeo.Player(iframe);
 
         // When the player is ready, add listeners for pause, finish,
