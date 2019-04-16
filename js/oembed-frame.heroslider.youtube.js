e87199dabc49c068f01994e1430a73131b6def82
Rajab Natshah <rajabn@gmail.com>
2019-04-16 15:43:41 +0300

    Added www. to youtube.com/player_api in the insert script tag

--- js/oembed-frame.heroslider.youtube.js
+++ js/oembed-frame.heroslider.youtube.js
@@ -17,7 +17,7 @@ function ready(fn) {
 }
 
 var tag = document.createElement('script');
-tag.src = "//youtube.com/player_api";
+tag.src = "//www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
