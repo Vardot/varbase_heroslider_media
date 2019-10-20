69c740a16542b2f2f3e791b3fc32a56898469608
FirassZiedan <FirassZiedan@3559482.no-reply.drupal.org>
2019-10-20 11:15:49 +0300

    Issue #3088950 by Firass Ziedan: Change Youtube js file structure in hero slider to make it work better and avoid some issues

--- js/video.heroslider.youtube.js
+++ js/video.heroslider.youtube.js
@@ -8,56 +8,54 @@
 
   Drupal.behaviors.varbaseHeroSliderMedia_youtube = {
     attach: function (context, settings) {
-      $(window).on('load', function () {
-        // On before slide change.
-        $('.slick--view--varbase-heroslider-media .slick__slider', context).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-          var currentSlideObject = $('.slide--' + currentSlide + '.slick-active');
-          var nextSlideObject = $('.slide--' + nextSlide);
-          var currentVideo = currentSlideObject.find('.varbase-video-player iframe[src*="youtube.com"]', context);
-          var nextVideo = nextSlideObject.find('.varbase-video-player iframe[src*="youtube.com"]', context);
-
-          if (currentVideo.length > 0) {
-            var currentPlayer = currentVideo.get(0).contentWindow;
-            currentPlayer.postMessage('pause', "*");
-          }
-
-          if (nextVideo.length > 0) {
-            var nextPlayer = nextVideo.get(0).contentWindow;
-            nextPlayer.postMessage('play', "*");
-          }
-          else {
-            $('.slick__slider').slick('slickPlay');
-          }
-        });
-
-        // When first slide has a video (Pause the slider and play the video).
-        $('.slick--view--varbase-heroslider-media', context).each(function () {
-          var firstIframeVideo = $(this).find('.slide').first().find('.varbase-video-player iframe[src*="youtube.com"]', context);
-          if (firstIframeVideo.length > 0) {
-            $('.slick__slider').slick('slickPause');
-            var firstIframeVideoPlayer = firstIframeVideo.get(0).contentWindow;
-            firstIframeVideoPlayer.postMessage('play', "*");
-          }
-        });
-
-        function youtubeActionProcessor(e) {
-          if (e.data === "endedYoutube" || e.message === "endedYoutube") {
-            $('.slick__slider').slick('slickNext');
-          }
-          else {
-            $('.slick__slider').slick('slickPause');
-          }
+      var mediaSliders = $('.slick--view--varbase-heroslider-media .slick__slider', context);
+      // On before slide change.
+      mediaSliders.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        var currentVideo = $('.slide--' + currentSlide + '.slick-active').find('.varbase-video-player iframe[src*="youtube.com"]', context);
+        if (currentVideo.length > 0) {
+          currentVideo.get(0).contentWindow.postMessage('pause', "*");
         }
+      });
 
-        // Setup the event listener for messaging.
-        if (window.addEventListener) {
-          window.addEventListener("message", youtubeActionProcessor, false);
+      // On after slide change.
+      mediaSliders.on('afterChange', function (event, slick, currentSlide) {
+        var currentVideo = $('.slide--' + currentSlide + '.slick-active').find('.varbase-video-player iframe[src*="youtube.com"]', context);
+        if (currentVideo.length > 0) {
+          currentVideo.get(0).contentWindow.postMessage('play', "*");
         }
         else {
-          window.attachEvent("onmessage", youtubeActionProcessor);
+          $('.slick__slider').slick('slickPlay');
         }
-
       });
+
+      // On first slide load.
+      mediaSliders.each(function(i) {
+        var firstIframeVideo = $(this).find('.slide').first().find('.varbase-video-player iframe[src*="youtube.com"]', context);
+        if (firstIframeVideo.length > 0) {
+          firstIframeVideo.on("load", function() {
+            $('.slick__slider').slick('slickPause');
+            $(this).get(0).contentWindow.postMessage('play', "*");
+          });
+        }
+      });
+
+      function youtubeActionProcessor(e) {
+        if (e.data === "endedYoutube" || e.message === "endedYoutube") {
+          $('.slick__slider').slick('slickNext');
+        }
+        else {
+          $('.slick__slider').slick('slickPause');
+        }
+      }
+
+      // Setup the event listener for messaging.
+      if (window.addEventListener) {
+        window.addEventListener("message", youtubeActionProcessor, false);
+      }
+      else {
+        window.attachEvent("onmessage", youtubeActionProcessor);
+      }
+
     }
   };
 })(window.jQuery, window.Drupal);
