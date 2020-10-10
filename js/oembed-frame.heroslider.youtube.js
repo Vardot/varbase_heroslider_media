ec05b01201c4abd46eba1b41e0a657ee7b4381a9
Rajab Natshah <rajabn@gmail.com>
2020-10-10 15:04:15 +0300

    Issue #3171132: Add yarn script commands for Drupal coding standard and Drupal Practice for Varbase Media Hero Slider

--- js/oembed-frame.heroslider.youtube.js
+++ js/oembed-frame.heroslider.youtube.js
@@ -4,56 +4,56 @@
  */
 
 function ready(fn) {
-  if (document.readyState != 'loading') {
+  if (document.readyState !== "loading") {
     fn();
   } else if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', fn);
+    document.addEventListener("DOMContentLoaded", fn);
   } else {
-    document.attachEvent('onreadystatechange', function() {
-      if (document.readyState != 'loading') {
+    document.attachEvent("onreadystatechange", function() {
+      if (document.readyState !== "loading") {
         fn();
       }
     });
   }
 }
 
-const tag = document.createElement('script');
-tag.src = '//www.youtube.com/player_api';
-const firstScriptTag = document.getElementsByTagName('script')[0];
+const tag = document.createElement("script");
+tag.src = "//www.youtube.com/player_api";
+const firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 ready(function() {
-  const media_iframe = document.querySelector('iframe');
-  media_iframe.setAttribute('id', 'media-oembed-iframe');
+  const mediaIframe = document.querySelector("iframe");
+  mediaIframe.setAttribute("id", "media-oembed-iframe");
 
-  let player_confgured = false;
-  let youtube_player;
+  let playerConfgured = false;
+  let youtubePlayer;
 
   function actionProcessor(evt) {
     // Manage Youtube video.
-    if (evt.data === 'play') {
-      const youtube_iframe = document.querySelector(
+    if (evt.data === "play") {
+      const youtubeIframe = document.querySelector(
         'iframe[src*="youtube.com"]'
       );
-      if (youtube_iframe !== undefined && youtube_iframe.src !== undefined) {
-        if (!player_confgured) {
-          let youtubeURL = String(youtube_iframe.src);
-          youtubeURL = youtubeURL.replace(/autoplay=0/gi, 'autoplay=1');
-          youtubeURL = youtubeURL.replace(/controls=1/gi, 'controls=0');
-          youtubeURL += '&controls=0';
-          youtubeURL += '&enablejsapi=1';
-          youtubeURL += '&showinfo=0'; // Hide the video title.
-          youtubeURL += '&modestbranding=1'; // Hide the Youtube Logo.
-          youtubeURL += '&loop=1'; // Run the video in a loop.
-          youtubeURL += '&fs=1'; // Hide the full screen button.
-          youtubeURL += '&cc_load_policy=1'; // Hide closed captions.
-          youtubeURL += '&iv_load_policy=1'; // Hide the Video Annotations.
-          youtubeURL += '&volume=0';
-          youtubeURL += '&rel=0';
-          youtube_iframe.src = youtubeURL;
+      if (youtubeIframe !== undefined && youtubeIframe.src !== undefined) {
+        if (!playerConfgured) {
+          let youtubeURL = String(youtubeIframe.src);
+          youtubeURL = youtubeURL.replace(/autoplay=0/gi, "autoplay=1");
+          youtubeURL = youtubeURL.replace(/controls=1/gi, "controls=0");
+          youtubeURL += "&controls=0";
+          youtubeURL += "&enablejsapi=1";
+          youtubeURL += "&showinfo=0"; // Hide the video title.
+          youtubeURL += "&modestbranding=1"; // Hide the Youtube Logo.
+          youtubeURL += "&loop=1"; // Run the video in a loop.
+          youtubeURL += "&fs=1"; // Hide the full screen button.
+          youtubeURL += "&cc_load_policy=1"; // Hide closed captions.
+          youtubeURL += "&iv_load_policy=1"; // Hide the Video Annotations.
+          youtubeURL += "&volume=0";
+          youtubeURL += "&rel=0";
+          youtubeIframe.src = youtubeURL;
           youtubeURL = undefined;
 
-          youtube_player = new YT.Player(youtube_iframe.id, {
+          youtubePlayer = new window.YT.Player(youtubeIframe.id, {
             playerVars: {
               autoplay: 1, // Auto-play the video on load.
               controls: 0, // Show pause/play buttons in player.
@@ -70,45 +70,45 @@ ready(function() {
             }
           });
 
-          function onPlayerReady(event) {
-            event.target.mute();
-            event.target.setVolume(0);
-            event.target.playVideo();
-          }
-
-          function onPlayerStateChange(event) {
-            if (event.data === YT.PlayerState.PLAYING) {
-              youtube_player.isPlaying = true;
-            } else {
-              youtube_player.isPlaying = false;
-            }
-
-            if (event.data === YT.PlayerState.ENDED) {
-              window.parent.postMessage('endedYoutube', '*');
-              youtube_player.pauseVideo();
-            } else {
-              window.parent.postMessage('playingYoutube', '*');
-            }
-          }
-
-          player_confgured = true;
+          playerConfgured = true;
         } else {
-          youtube_player.seekTo(0);
-          youtube_player.playVideo();
+          youtubePlayer.seekTo(0);
+          youtubePlayer.playVideo();
         }
       }
-    } else if (evt.data === 'pause') {
-      if (player_confgured) {
-        youtube_player.pauseVideo();
-        youtube_player.isPlaying = false;
+    } else if (evt.data === "pause") {
+      if (playerConfgured) {
+        youtubePlayer.pauseVideo();
+        youtubePlayer.isPlaying = false;
       }
+    }
+  }
+
+  function onPlayerReady(event) {
+    event.target.mute();
+    event.target.setVolume(0);
+    event.target.playVideo();
+  }
+
+  function onPlayerStateChange(event) {
+    if (event.data === window.YT.PlayerState.PLAYING) {
+      youtubePlayer.isPlaying = true;
+    } else {
+      youtubePlayer.isPlaying = false;
+    }
+
+    if (event.data === window.YT.PlayerState.ENDED) {
+      window.parent.postMessage("endedYoutube", "*");
+      youtubePlayer.pauseVideo();
+    } else {
+      window.parent.postMessage("playingYoutube", "*");
     }
   }
 
   // Setup the event listener for messaging.
   if (window.addEventListener) {
-    window.addEventListener('message', actionProcessor, false);
+    window.addEventListener("message", actionProcessor, false);
   } else {
-    window.attachEvent('onmessage', actionProcessor);
+    window.attachEvent("onmessage", actionProcessor);
   }
 });
--- js/video.heroslider.youtube.js
+++ js/video.heroslider.youtube.js
@@ -5,68 +5,68 @@
 
 (function($, Drupal) {
   Drupal.behaviors.varbaseHeroSliderMedia_youtube = {
-    attach: function(context, settings) {
+    attach: function(context) {
       const mediaSliders = $(
-        '.slick--view--varbase-heroslider-media .slick__slider',
+        ".slick--view--varbase-heroslider-media .slick__slider",
         context
       );
       // On before slide change.
-      mediaSliders.on('beforeChange', function(
+      mediaSliders.on("beforeChange", function(
         event,
         slick,
         currentSlide,
         nextSlide
       ) {
         const currentVideo = $(
-          '.slide--' + currentSlide + '.slick-active'
+          ".slide--" + currentSlide + ".slick-active"
         ).find('.varbase-video-player iframe[src*="youtube.com"]', context);
         if (currentVideo.length > 0) {
-          currentVideo.get(0).contentWindow.postMessage('pause', '*');
+          currentVideo.get(0).contentWindow.postMessage("pause", "*");
         }
       });
 
       // On after slide change.
-      mediaSliders.on('afterChange', function(event, slick, currentSlide) {
+      mediaSliders.on("afterChange", function(event, slick, currentSlide) {
         const currentVideo = $(
-          '.slide--' + currentSlide + '.slick-active'
+          ".slide--" + currentSlide + ".slick-active"
         ).find('.varbase-video-player iframe[src*="youtube.com"]', context);
         if (currentVideo.length > 0) {
-          currentVideo.get(0).contentWindow.postMessage('play', '*');
+          currentVideo.get(0).contentWindow.postMessage("play", "*");
         } else {
-          $('.slick__slider').slick('slickPlay');
+          $(".slick__slider").slick("slickPlay");
         }
       });
 
       // On first slide load.
-      const firstIframeVideo = $('.varbase-heroslider-media')
-        .find('.slide')
+      const firstIframeVideo = $(".varbase-heroslider-media")
+        .find(".slide")
         .first()
         .find('.varbase-video-player iframe[src*="youtube.com"]', context);
       if (firstIframeVideo.length > 0) {
-        firstIframeVideo.on('load', function() {
-          $('.slick__slider').slick('slickPause');
+        firstIframeVideo.on("load", function() {
+          $(".slick__slider").slick("slickPause");
           $(this)
             .get(0)
-            .contentWindow.postMessage('play', '*');
+            .contentWindow.postMessage("play", "*");
         });
       }
 
       function youtubeActionProcessor(e) {
-        if (e.data === 'endedYoutube' || e.message === 'endedYoutube') {
-          $('.slick__slider').slick('slickNext');
+        if (e.data === "endedYoutube" || e.message === "endedYoutube") {
+          $(".slick__slider").slick("slickNext");
         } else if (
-          e.data === 'playingYoutube' ||
-          e.message === 'playingYoutube'
+          e.data === "playingYoutube" ||
+          e.message === "playingYoutube"
         ) {
-          $('.slick__slider').slick('slickPause');
+          $(".slick__slider").slick("slickPause");
         }
       }
 
       // Setup the event listener for messaging.
       if (window.addEventListener) {
-        window.addEventListener('message', youtubeActionProcessor, false);
+        window.addEventListener("message", youtubeActionProcessor, false);
       } else {
-        window.attachEvent('onmessage', youtubeActionProcessor);
+        window.attachEvent("onmessage", youtubeActionProcessor);
       }
     }
   };
