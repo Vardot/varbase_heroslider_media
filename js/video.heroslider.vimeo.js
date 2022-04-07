aae158a3de0d1c7f8f1a202f09afd681dceb1438
abdallah-yassin <abdallah-yassin@3707588.no-reply.drupal.org>
2022-04-07 11:30:30 +0300

    Issue #3272442 by Abdullah Yassin: Switch from deprecated Node Sass to Dart Sass compiler using Gulp and change minimum node version to 16.0 and later in the Varbase Media Hero Slider module

--- js/video.heroslider.vimeo.js
+++ js/video.heroslider.vimeo.js
@@ -5,64 +5,66 @@
 
 (function ($, Drupal) {
   Drupal.behaviors.varbaseHeroSliderMedia_vimeo = {
-    attach: function (context) {
+    attach(context) {
       const mediaSliders = $(
-        ".slick--view--varbase-heroslider-media .slick__slider",
+        '.slick--view--varbase-heroslider-media .slick__slider',
         context
       );
       // On before slide change.
       mediaSliders.on(
-        "beforeChange",
+        'beforeChange',
         function (event, slick, currentSlide, nextSlide) {
-          const currentVideo = $(
-            ".slide--" + currentSlide + ".slick-active"
-          ).find('.varbase-video-player iframe[src*="vimeo.com"]', context);
+          const currentVideo = $(`.slide--${currentSlide}.slick-active`).find(
+            '.varbase-video-player iframe[src*="vimeo.com"]',
+            context
+          );
           if (currentVideo.length > 0) {
-            currentVideo.get(0).contentWindow.postMessage("pause", "*");
+            currentVideo.get(0).contentWindow.postMessage('pause', '*');
           }
         }
       );
 
       // On after slide change.
-      mediaSliders.on("afterChange", function (event, slick, currentSlide) {
-        const currentVideo = $(
-          ".slide--" + currentSlide + ".slick-active"
-        ).find('.varbase-video-player iframe[src*="vimeo.com"]', context);
+      mediaSliders.on('afterChange', function (event, slick, currentSlide) {
+        const currentVideo = $(`.slide--${currentSlide}.slick-active`).find(
+          '.varbase-video-player iframe[src*="vimeo.com"]',
+          context
+        );
         if (currentVideo.length > 0) {
-          currentVideo.get(0).contentWindow.postMessage("play", "*");
+          currentVideo.get(0).contentWindow.postMessage('play', '*');
         } else {
-          mediaSliders.slick("slickPlay");
+          mediaSliders.slick('slickPlay');
         }
       });
 
       // On first slide load.
-      const firstIframeVideo = $(".varbase-heroslider-media")
-        .find(".slide")
+      const firstIframeVideo = $('.varbase-heroslider-media')
+        .find('.slide')
         .first()
         .find('.varbase-video-player iframe[src*="vimeo.com"]', context);
       if (firstIframeVideo.length > 0) {
-        firstIframeVideo.on("load", function () {
-          if (!firstIframeVideo.hasClass("first-slide-played")) {
-            mediaSliders.slick("slickPause");
-            $(this).get(0).contentWindow.postMessage("play", "*");
-            firstIframeVideo.addClass("first-slide-played");
+        firstIframeVideo.on('load', function () {
+          if (!firstIframeVideo.hasClass('first-slide-played')) {
+            mediaSliders.slick('slickPause');
+            $(this).get(0).contentWindow.postMessage('play', '*');
+            firstIframeVideo.addClass('first-slide-played');
           }
         });
       }
 
       function vimeoActionProcessor(e) {
-        if (e.data === "endedVimeo" || e.message === "endedVimeo") {
-          mediaSliders.slick("slickNext");
-        } else if (e.data === "playingVimeo" || e.message === "playingVimeo") {
-          mediaSliders.slick("slickPause");
+        if (e.data === 'endedVimeo' || e.message === 'endedVimeo') {
+          mediaSliders.slick('slickNext');
+        } else if (e.data === 'playingVimeo' || e.message === 'playingVimeo') {
+          mediaSliders.slick('slickPause');
         }
       }
 
       // Setup the event listener for messaging.
       if (window.addEventListener) {
-        window.addEventListener("message", vimeoActionProcessor, false);
+        window.addEventListener('message', vimeoActionProcessor, false);
       } else {
-        window.attachEvent("onmessage", vimeoActionProcessor);
+        window.attachEvent('onmessage', vimeoActionProcessor);
       }
     }
   };
--- js/video.heroslider.youtube.js
+++ js/video.heroslider.youtube.js
@@ -5,64 +5,66 @@
 
 (function ($, Drupal) {
   Drupal.behaviors.varbaseHeroSliderMedia_youtube = {
-    attach: function (context) {
+    attach(context) {
       const mediaSliders = $(
-        ".slick--view--varbase-heroslider-media .slick__slider",
+        '.slick--view--varbase-heroslider-media .slick__slider',
         context
       );
       // On before slide change.
       mediaSliders.on(
-        "beforeChange",
+        'beforeChange',
         function (event, slick, currentSlide, nextSlide) {
-          const currentVideo = $(
-            ".slide--" + currentSlide + ".slick-active"
-          ).find('.varbase-video-player iframe[src*="youtube.com"]', context);
+          const currentVideo = $(`.slide--${currentSlide}.slick-active`).find(
+            '.varbase-video-player iframe[src*="youtube.com"]',
+            context
+          );
           if (currentVideo.length > 0) {
-            currentVideo.get(0).contentWindow.postMessage("pause", "*");
+            currentVideo.get(0).contentWindow.postMessage('pause', '*');
           }
         }
       );
 
       // On after slide change.
-      mediaSliders.on("afterChange", function (event, slick, currentSlide) {
-        const currentVideo = $(
-          ".slide--" + currentSlide + ".slick-active"
-        ).find('.varbase-video-player iframe[src*="youtube.com"]', context);
+      mediaSliders.on('afterChange', function (event, slick, currentSlide) {
+        const currentVideo = $(`.slide--${currentSlide}.slick-active`).find(
+          '.varbase-video-player iframe[src*="youtube.com"]',
+          context
+        );
         if (currentVideo.length > 0) {
-          currentVideo.get(0).contentWindow.postMessage("play", "*");
+          currentVideo.get(0).contentWindow.postMessage('play', '*');
         } else {
-          $(".slick__slider").slick("slickPlay");
+          $('.slick__slider').slick('slickPlay');
         }
       });
 
       // On first slide load.
-      const firstIframeVideo = $(".varbase-heroslider-media")
-        .find(".slide")
+      const firstIframeVideo = $('.varbase-heroslider-media')
+        .find('.slide')
         .first()
         .find('.varbase-video-player iframe[src*="youtube.com"]', context);
       if (firstIframeVideo.length > 0) {
-        firstIframeVideo.on("load", function () {
-          $(".slick__slider").slick("slickPause");
-          $(this).get(0).contentWindow.postMessage("play", "*");
+        firstIframeVideo.on('load', function () {
+          $('.slick__slider').slick('slickPause');
+          $(this).get(0).contentWindow.postMessage('play', '*');
         });
       }
 
       function youtubeActionProcessor(e) {
-        if (e.data === "endedYoutube" || e.message === "endedYoutube") {
-          $(".slick__slider").slick("slickNext");
+        if (e.data === 'endedYoutube' || e.message === 'endedYoutube') {
+          $('.slick__slider').slick('slickNext');
         } else if (
-          e.data === "playingYoutube" ||
-          e.message === "playingYoutube"
+          e.data === 'playingYoutube' ||
+          e.message === 'playingYoutube'
         ) {
-          $(".slick__slider").slick("slickPause");
+          $('.slick__slider').slick('slickPause');
         }
       }
 
       // Setup the event listener for messaging.
       if (window.addEventListener) {
-        window.addEventListener("message", youtubeActionProcessor, false);
+        window.addEventListener('message', youtubeActionProcessor, false);
       } else {
-        window.attachEvent("onmessage", youtubeActionProcessor);
+        window.attachEvent('onmessage', youtubeActionProcessor);
       }
     }
   };
