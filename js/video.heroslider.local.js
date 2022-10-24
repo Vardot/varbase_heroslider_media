a1a80bde4a0fabf614b8c5172df3f37b6b515a84
Mohammad Hawwari <27047-mhawwari@users.noreply.drupalcode.org>
2022-10-24 15:03:45 +0300

    Issue #3102942 by mhawwari, mahseri, Qusai Taha: Change Hero Slider to use a specific slider selector

--- js/video.heroslider.local.js
+++ js/video.heroslider.local.js
@@ -7,40 +7,42 @@
   Drupal.behaviors.varbaseHeroSliderMedia_local_video = {
     attach(context) {
       $(window).on('load', function () {
-        // On before slide change.
-        $('.slick--view--varbase-heroslider-media .slick__slider', context).on(
-          'beforeChange',
-          function (event, slick, currentSlide, nextSlide) {
-            const currentSlideObject = $(
-              `.slide--${currentSlide}.slick-active`
-            );
-            const nextSlideObject = $(`.slide--${nextSlide}`);
-            const currentVideo = currentSlideObject.find(
-              '.varbase-video-player video',
-              context
-            );
-            const nextVideo = nextSlideObject.find(
-              '.varbase-video-player video',
-              context
-            );
-
-            if (currentVideo.length > 0) {
-              const currentPlayer = currentVideo.get(0);
-              currentPlayer.pause();
-            }
-
-            if (nextVideo.length > 0) {
-              const nextPlayer = nextVideo.get(0);
-              nextPlayer.muted = true;
-              nextPlayer.onpause = onPause;
-              nextPlayer.onended = onFinish;
-              nextPlayer.onplay = onPlayProgress;
-              nextPlayer.play();
-            } else {
-              $('.slick__slider').slick('slickPlay');
-            }
-          }
+        const mediaSliders = $(
+          '.slick--view--varbase-heroslider-media .slick__slider',
+          context
         );
+
+        // On before slide change.
+        mediaSliders.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+          const currentSlideObject = $(
+            '.slide--' + currentSlide + '.slick-active'
+          );
+          const nextSlideObject = $('.slide--' + nextSlide);
+          const currentVideo = currentSlideObject.find(
+            '.varbase-video-player video',
+            context
+          );
+          const nextVideo = nextSlideObject.find(
+            '.varbase-video-player video',
+            context
+          );
+
+          if (currentVideo.length > 0) {
+            const currentPlayer = currentVideo.get(0);
+            currentPlayer.pause();
+          }
+
+          if (nextVideo.length > 0) {
+            const nextPlayer = nextVideo.get(0);
+            nextPlayer.muted = true;
+            nextPlayer.onpause = onPause;
+            nextPlayer.onended = onFinish;
+            nextPlayer.onplay = onPlayProgress;
+            nextPlayer.play();
+          } else {
+            mediaSliders.slick('slickPlay');
+          }
+        });
 
         // When first slide has a video (Pause the slider and play the video).
         $('.slick--view--varbase-heroslider-media', context).each(function () {
@@ -49,14 +51,14 @@
             .find('.varbase-video-player video', context);
 
           if (firstVideo.length > 0) {
-            $('.slick__slider').slick('slickPause');
+            mediaSliders.slick('slickPause');
 
             const firstVideoPlayer = firstVideo.get(0);
             firstVideoPlayer.muted = true;
             firstVideoPlayer.play();
 
             firstVideo.on('ended', function () {
-              $('.slick__slider').slick('slickPlay');
+              mediaSliders.slick('slickPlay');
             });
           }
         });
@@ -82,17 +84,17 @@
 
         // Play when paused.
         function onPause() {
-          $('.slick__slider').slick('slickNext');
+          mediaSliders.slick('slickNext');
         }
 
         // Play when finished.
         function onFinish() {
-          $('.slick__slider').slick('slickPlay');
+          mediaSliders.slick('slickPlay');
         }
 
         // Pause on play prgress.
         function onPlayProgress() {
-          $('.slick__slider').slick('slickPause');
+          mediaSliders.slick('slickPause');
         }
       });
     }
